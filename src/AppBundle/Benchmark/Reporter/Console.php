339e4a1e22753f01046217eea5d020de24b602e7
Jacek Lange <jacek.lange@gmail.com>
2017-09-04 12:16:09 +0200

    columns in console

--- src/AppBundle/Benchmark/Reporter/Console.php
+++ src/AppBundle/Benchmark/Reporter/Console.php
@@ -5,14 +5,23 @@ namespace AppBundle\Benchmark\Reporter;
 
 use AppBundle\Benchmark\Reporter;
 use AppBundle\Dto\TestResult;
+use AppBundle\Dto\Url;
 use AppBundle\Dto\WebsiteResult;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
 
 
-class Console implements Reporter
+class Console implements Reporter, EventSubscriberInterface
 {
     /** @var LoggerInterface */
     private $logger;
+
+    /** @var TestResult[] */
+    private $testResults = [];
+
+    private $currentLine = '';
 
     /**
      * @param LoggerInterface $logger
@@ -24,26 +33,114 @@ class Console implements Reporter
 
     public function report(TestResult $result)
     {
-        $this->logger->info('Benchmark started at: ' .
+        $this->testResults[] = $result;
+        $this->printStr('Benchmark started at: ' .
             date('Y-m-d H:i:sP', $result->getStartTimestamp()));
+        $this->finishRow();
+
+
+
+        /*
         $this->logger->info($result->getName());
         $this->printSingleResult($result->getUnit(), $result->getMainWebsiteResult());
 
-        $this->logger->info('Competition:');
+        $this->printStr('Competition:');
         foreach($result->getWebsiteResults() as $otherResult) {
             $this->printSingleResult($result->getUnit(), $otherResult);
         }
+        */
     }
 
-    private function printSingleResult(string $unit, WebsiteResult $websiteResult)
+    private function printSingleResult(string $testName, string $unit, WebsiteResult $websiteResult)
     {
-        $this->logger->info(
-            sprintf("%d %s\t%s",
+        $this->printStr(
+            sprintf("%s: %d %s",
+                $testName,
                 $websiteResult->getValue(),
-                $unit,
-                $websiteResult->getUrl()->getUrl()
+                $unit
             )
         );
     }
 
+    public function onException()
+    {
+        $this->wasExceptionThrown = true;
+    }
+
+    public function onTerminate()
+    {
+        if (!$this->testResults) {
+
+            return;
+        }
+
+        $this->printHeader();
+
+        $this->printUrl($this->testResults[0]->getMainWebsiteResult()->getUrl());
+        foreach($this->testResults as $testResult) {
+            $this->printSeparator();
+            $this->printSingleResult($testResult->getName(), $testResult->getUnit(), $testResult->getMainWebsiteResult());
+
+        }
+        $this->finishRow();
+
+        foreach($this->testResults[0]->getWebsiteResults() as $websiteResult) {
+            $this->printUrl($websiteResult->getUrl());
+            foreach($this->testResults as $testResult) {
+                $this->printSeparator();
+                $this->printSingleResult(
+                    $testResult->getName(),
+                    $testResult->getUnit(),
+                    // TODO: move getByUrl to testresult?
+                    $testResult->getWebsiteResults()->getByUrl($websiteResult->getUrl())
+                );
+            }
+            $this->finishRow();
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        $listeners = array(
+            KernelEvents::EXCEPTION => 'onException',
+            KernelEvents::TERMINATE => 'onTerminate'
+        );
+
+        if (class_exists('Symfony\Component\Console\ConsoleEvents')) {
+            $listeners[class_exists('Symfony\Component\Console\Event\ConsoleErrorEvent') ? ConsoleEvents::ERROR :  ConsoleEvents::EXCEPTION] = 'onException';
+            $listeners[ConsoleEvents::TERMINATE] = 'onTerminate';
+        }
+
+        return $listeners;
+    }
+
+    private function printHeader()
+    {
+        foreach($this->testResults as $testResult) {
+            $this->printSeparator();
+            $this->printStr($testResult->getName());
+        }
+        $this->finishRow();
+    }
+
+    private function printUrl(Url $getUrl)
+    {
+        $this->printStr($getUrl->getUrl());
+    }
+
+    private function printSeparator()
+    {
+        $this->printStr("\t");
+    }
+
+    private function printStr($string)
+    {
+        $this->currentLine .= $string;
+    }
+
+    private function finishRow()
+    {
+        $this->logger->info($this->currentLine);
+        $this->currentLine = '';
+    }
 }
